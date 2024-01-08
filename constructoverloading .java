8013673829b1ed32d1b9555fc47d5944b9b232e3
mohan998877 <152840076+mohan998877@users.noreply.github.com>
2024-01-08 11:13:44 +0530

    constructionoverloading

---
+++ constructoverloading .java
@@ -0,0 +1,19 @@
+class ConstructorOverloading 
+{
+	
+   ConstructorOverloading() 
+   { 
+     System.out.println("Hello, I am a Constructor with no parameter.");
+   }
+
+   ConstructorOverloading(String name) 
+   {
+     System.out.println("Hello, I am a Constructor with one parameter, name = " + name);	
+   }
+
+public static void main(String[] args) 
+	{
+  		ConstructorOverloading p1 = new ConstructorOverloading();
+  		ConstructorOverloading p2 = new ConstructorOverloading("Dr. Saket"); 
+	}
+}  
